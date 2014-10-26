45523671ada10b51ab84d552fde5f6b3798d8e50
alikefool <xaligoox@gmail.com>
2014-10-26 11:19:22 +0800

    actionBar demo

--- app/src/main/java/com/aliku/firstapp/alikefirstapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/aliku/firstapp/alikefirstapplication/DisplayMessageActivity.java
@@ -1,18 +1,15 @@
 package com.aliku.firstapp.alikefirstapplication;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.LayoutInflater;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.app.Fragment;
 
 
-public class DisplayMessageActivity extends Activity {
+public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +28,19 @@ public class DisplayMessageActivity extends Activity {
         // Set the text view as the activity layout
         setContentView(textView);
 
+        // If your minSdkVersion is 11 or higher, instead use:
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.display_message, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.display_message, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -45,11 +48,25 @@ public class DisplayMessageActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean openSearch () {
+        return true;
+    }
+
+    private boolean openSettings () {
+        return true;
     }
 
     /**
